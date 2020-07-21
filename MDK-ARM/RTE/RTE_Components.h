fb448538a29b457807fa3ae8d8d021f9ccf62a96
aronno <tebunia@gmail.com>
2020-07-22 02:00:29 +0600

    first commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Capture01' 
+ * Target:  'Capture01' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
